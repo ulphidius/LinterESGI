ce340bea23c810897596be21fc7712c245a4a5bb
Ceyf <44698315+Ceyf@users.noreply.github.com>
2018-12-06 14:57:25 +0100

    Upload de no-multi-declaractions.c & unusedVariables.c
    no-multi-declarations presque fini, seul problème restant: l'affichage des erreur peut se faire dans le mauvais sens
    
    unusedVariables en cours, problèmes de pointeurs qui freinent
    
    Manque les headers (commentaires)

---
+++ sources/no-multi-declarations.c
@@ -0,0 +1,54 @@
+int noMultiDeclarations(){
+
+int err = 0, err2 = 0;
+    int pointVirgule = 0;
+    int lineN = 1;
+    char* line = malloc(sizeof(char) * 50);
+    char* word = malloc(sizeof(char) * 50);
+
+    FILE* f = fopen("test.c", "w");
+    fprintf(f, "int a;\nint b;\nint c;int d;");
+    fclose(f);
+    f = fopen("test.c", "r");
+
+    while(fgets(line, 50, f)){
+        word = strtok(line, " ");
+        if(strcmp(word, "int") == 0 || strcmp(word, "char") == 0 || strcmp(word, "long") == 0 || strcmp(word, "short") == 0 || strcmp(word, "float") == 0 || strcmp(word, "double") == 0 || strcmp(word, "const") == 0){
+            while(word !=  NULL){
+                if(strchr(word, ',') != NULL){
+                    printf("\nErreur: Multiples declarations de variables sur la ligne %d\n", lineN);
+                    err++;
+                }
+                word = strtok(NULL, " ");
+        }
+        }
+        lineN++;
+    }
+
+
+    lineN = 1;
+    rewind(f);
+    while(fgets(line, 50, f)){
+        word = strtok(line, " ");
+        if(strcmp(word, "int") == 0 || strcmp(word, "char") == 0 || strcmp(word, "long") == 0 || strcmp(word, "short") == 0 || strcmp(word, "float") == 0 || strcmp(word, "double") == 0 || strcmp(word, "const") == 0){
+            while(word !=  NULL){
+                word = str(line, ";");
+                printf("word = %c\n", word[strlen(word) - 1]);
+                if(word[1] == '\n')
+                    break;
+                if(strcmp(word, "int") == 0 || strcmp(word, "char") == 0 || strcmp(word, "long") == 0 || strcmp(word, "short") == 0 || strcmp(word, "float") == 0 || strcmp(word, "double") == 0 || strcmp(word, "const") == 0){
+                    printf("Erreur: Multiples declarations de variables sur la ligne %d\n", lineN);
+                    err++;
+                }
+                word = strtok(NULL, " ");
+        }
+        }
+        lineN++;
+    }
+    
+    fclose(f);
+    free(line);
+    free(word);
+    return err;
+    return 0;
+}
---
+++ sources/variables.h
@@ -0,0 +1,38 @@
+#ifndef VARIABLES_H_INCLUDED
+#define VARIABLES_H_INCLUDED
+
+/*
+**  Filename : variables.h
+**
+**  Made by : TeamDev
+**
+**  Description :
+*/
+
+/**
+ * Lib
+ **/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * Define
+ **/
+
+
+/**
+ * Structure
+ **/
+
+
+
+/**
+ * Functions
+ **/
+int unusedVariables();
+int noMultiDeclarations();
+
+#endif // VARIABLES_H_INCLUDED
